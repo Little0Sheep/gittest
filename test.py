a37579370ad5ba4b22928b2c046271e946321bdb
apg <997318740@qq.com>
2017-07-06 21:27:08 +0800

    add a message

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-import matp
+import matplotlib.pyplot as plt
 
 
 flags=tf.app.flags
@@ -22,21 +22,28 @@ train_step=tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 init=tf.initialize_all_variables()
 sess=tf.InteractiveSession()
 sess.run(init)
-for i in range(100):
+for i in range(1000):
     batch_xs,batch_ys=mnist.train.next_batch(100)
     sess.run(train_step,feed_dict={x:batch_xs,y_:batch_ys})
-    # print(batch_ys[0])
-    # print(sess.run(y,feed_dict={x:batch_xs,y_:batch_ys})[0][9])
 
+x1=[]
+y1=[]
+index=668;
+x1.append(mnist.test.images[index])
+y1.append(mnist.test.labels[index])
+# print(sess.run(tf.equal(tf.argmax(y,1),tf.argmax(y_,1)),feed_dict={x:batch_xs,y_:batch_ys}))
+print("识别出图片中的数字为：",sess.run(tf.argmax(y,1)[0],feed_dict={x:x1}))
 correct_prediction=tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
-print(mnist.test.images[0][783])
 accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-print(accuracy.eval({x:mnist.test.images,y_:mnist.test.labels}))
-batch_xs,batch_ys=mnist.train.next_batch(1)
-print(batch_ys[0])
-print(batch_xs[0])
-Image.fromarray(batch_xs[0])
+print("准确率：%.2f%%" % (accuracy.eval({x:mnist.test.images,y_:mnist.test.labels})*100))
 
+plt.imshow(np.array(x1[0]).reshape(28,28),plt.cm.gray)
+plt.show()
+# Image.fromarray(np.array(batch_xs[0]).reshape(28,28)).convert("L").save("2.jpg")
+# plt.figure()
+# plt.plot()
+# plt.imshow(np.array(batch_xs[0]).reshape(28,28))
+# plt.show()
 # batch_xs,batch_ys=mnist.train.next_batch(2)
 # print(batch_xs[0][783])
 # print(batch_ys[0][9])
